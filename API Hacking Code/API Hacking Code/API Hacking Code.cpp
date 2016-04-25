e2f2ff6ac6297ad873448a2481626b703b0a34be
Maddie <mk186442@falmouth.ac.uk>
2016-04-25 12:47:29 +0100

    It can now get JSON data and extract it
    Using a different tutorial that doesn't involve saving data to a file to
    use it

--- API Hacking Code/API Hacking Code/API Hacking Code.cpp
+++ API Hacking Code/API Hacking Code/API Hacking Code.cpp
@@ -1,6 +1,8 @@
 #include"stdafx.h"
 #include "cpprest/http_client.h"
 #include "cpprest/filestream.h"
+#include <iostream>
+#include <cpprest/json.h>
 
 using namespace utility;                    // Common utilities like string conversions
 using namespace web;                        // Common features like URIs.
@@ -8,52 +10,60 @@ using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
 using namespace concurrency::streams;       // Asynchronous streams
 
-int main(int argc, char* argv[])
+
+// Retrieves a JSON value from an HTTP request.
+pplx::task<void> RequestJSONValueAsync()
 {
-	//Using the casablanca Http Client Tutorial, currently just goes to the open notify page and saves it 
-	
-	//TODO: 
-	//Make a JSON object
-	//Handle results using JSON
-	//Intergrate with COMP150 game
-
-	auto fileStream = std::make_shared<ostream>();
-
-	// Open stream to output file.
-	pplx::task<void> requestTask = fstream::open_ostream(U("ISS_location.html")).then([=](ostream outFile)
+	// Tutorials used
+	//	http://www.drdobbs.com/tools/json-and-the-microsoft-c-rest-sdk/240164821
+	//  https://msdn.microsoft.com/en-us/library/jj950082.aspx
+	http_client client(L"http://api.open-notify.org/iss-now.json");
+	return client.request(methods::GET).then([](http_response response) -> pplx::task<json::value>
 	{
-		*fileStream = outFile;
-
-		// Create http_client to send the request.
-		http_client client(U("http://api.open-notify.org/iss-now.json"));
-
-		//Gets reults
-		return client.request(methods::GET);
+		if (response.status_code() == status_codes::OK)
+		{
+			return response.extract_json();
+		}
+		return pplx::task_from_result(json::value());
 	})
-
-		// Handle response headers arriving.
-		.then([=](http_response response)
+		.then([](pplx::task<json::value> previousTask)
 	{
-		printf("Received response status code:%u\n", response.status_code());
-
-		// Write response body into the file.
-		return response.body().read_to_end(fileStream->streambuf());
-	})
-
-		// Close the file stream
-		.then([=](size_t)
-	{
-		return fileStream->close();
+		try
+		{
+			const json::value& v = previousTask.get();
+			// JSON object
+		}
+		catch (const http_exception& e)
+		{
+			// Error catch
+			std::wostringstream ss;
+			ss << e.what() << std::endl;
+			std::wcout << ss.str();
+		}
 	});
+}
 
-	try
-	{
-		requestTask.wait();
-	}
-	catch (const std::exception &e)
-	{
-		printf("Error exception:%s\n", e.what());
-	}
+void IterateJSONValue()
+{
+	// JSON object
+	json::value iss_position;
+	iss_position[L"latitude"]; // = json::value::number(1);
+	iss_position[L"latitude"]; // = json::value::number(1);
 
-	return 0;
+	for (auto iter = iss_position.as_object().cbegin(); iter != iss_position.as_object().cend(); ++iter)
+	{
+		const std::wstring &str = iter->first;
+		const json::value &v = iter->second;
+
+		std::wcout << L"String: " << str << L", Value: " << v.serialize() << std::endl;
+	}
+}
+
+int wmain()
+{
+	std::wcout << L"Calling RequestJSONValueAsync..." << std::endl;
+	RequestJSONValueAsync().wait();
+
+	//std::wcout << L"Calling IterateJSONValue..." << std::endl;
+	//IterateJSONValue();
 }
