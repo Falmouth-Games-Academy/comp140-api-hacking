6441502c5ca5bd62397eae0ac84395d1253d1250
Maddie <maddiek19@outlook.com>
2016-04-25 16:48:28 +0100

    Can get specific data depending on the key

--- API Hacking Code/API Hacking Code/API Hacking Code.cpp
+++ API Hacking Code/API Hacking Code/API Hacking Code.cpp
@@ -10,7 +10,11 @@ using namespace web;                        // Common features like URIs.
 using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
 using namespace concurrency::streams;       // Asynchronous streams
+int currentTime;
 
+/* Tutorials used
+- http://www.drdobbs.com/tools/json-and-the-microsoft-c-rest-sdk/240164821
+- https://msdn.microsoft.com/en-us/library/jj950082.aspx */
 
 void DisplayJSONValue(json::value ISSData)
 {
@@ -18,28 +22,46 @@ void DisplayJSONValue(json::value ISSData)
 	{
 		for (auto iter = ISSData.as_object().cbegin(); iter != ISSData.as_object().cend(); ++iter)
 		{
-			// It is necessary to make sure that you get the value as const reference
-			// in order to avoid copying the whole JSON value recursively (too expensive for nested objects)
 			const json::value &key = json::value::string(iter->first);
 			const json::value &value = iter->second;
-
+			
 			if (value.is_object() || value.is_array())
 			{
-				// We have an object with children or an array
 				if ((!key.is_null()) && (key.is_string()))
 				{
 					std::wcout << L"Parent: " << key.as_string() << std::endl;
+					//Parent is the object in this case iss_position
+					//Parent contains the latitude & longitude
 				}
-				// Loop over each element in the object by calling DisplayJSONValue
 				DisplayJSONValue(value);
 				if ((!key.is_null()) && (key.is_string()))
 				{
 					std::wcout << L"End of Parent: " << key.as_string() << std::endl;
+					//break; //break makes it only get the  latitude & longitude and not the message on timestamp
+					//Probably need timestamp to choose when to update data
 				}
 			}
 			else
 			{
-				// Always display the value as a string
+
+				if (key.serialize() == (L"\"timestamp""\""))
+				{
+					std::wcout << L"timestamp found"<< std::endl;
+					// TODO: Save time
+					// currentTime = value.serialize();
+				}
+				else if (key.serialize() == (L"\"longitude""\""))
+				{
+					std::wcout << L"latitude found" << std::endl;
+					// TODO: Save latitude
+					// latitude = value.serialize();
+				}
+				else if (key.serialize() == (L"\"latitude""\""))
+				{
+					std::wcout << L"latitude found" << std::endl;
+					// TODO: Save latitude
+					// latitude = value.serialize();
+				}
 				std::wcout << L"Key: " << key.serialize() << L", Value: " << value.serialize() << std::endl;
 			}
 		}
@@ -50,11 +72,6 @@ void DisplayJSONValue(json::value ISSData)
 // Retrieves a JSON value from an HTTP request.
 pplx::task<void> RequestJSONValueAsync()
 {
-	/* Tutorials used
-	- http://www.drdobbs.com/tools/json-and-the-microsoft-c-rest-sdk/240164821
-	- https://msdn.microsoft.com/en-us/library/jj950082.aspx */
-
-
 	http_client client(L"http://api.open-notify.org/iss-now.json");
 	return client.request(methods::GET).then([](http_response response) -> pplx::task<json::value>
 	{
@@ -70,9 +87,8 @@ pplx::task<void> RequestJSONValueAsync()
 		{
 			const json::value& v = previousTask.get();
 			// JSON object
-			std::cout << "Success" << std::endl;
 			DisplayJSONValue(v);
-			std::cout << "Success 2" << std::endl;
+			std::cout << "Success" << std::endl;
 		}
 		catch (const http_exception& e)
 		{
